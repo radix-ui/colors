1fca778b3788c8cd91473d472cd6ee39a6b4ae32
Vlad Moroz <hi@vladmoroz.com>
2021-05-25 13:35:19 +0300

    Light theme alpha scales

---
+++ src/light/crimsonA.ts
@@ -0,0 +1,14 @@
+export const crimsonA = {
+  crimsonA1: 'hsl(340 100% 51.0% / 0.012)',
+  crimsonA2: 'hsl(330 100% 51.0% / 0.032)',
+  crimsonA3: 'hsl(332 99.1% 47.1% / 0.063)',
+  crimsonA4: 'hsl(331 99.9% 44.3% / 0.102)',
+  crimsonA5: 'hsl(333 99.9% 42.3% / 0.153)',
+  crimsonA6: 'hsl(333 99.5% 40.5% / 0.224)',
+  crimsonA7: 'hsl(335 99.7% 39.1% / 0.322)',
+  crimsonA8: 'hsl(336 99.5% 38.5% / 0.440)',
+  crimsonA9: 'hsl(336 99.9% 44.3% / 0.761)',
+  crimsonA10: 'hsl(336 100% 42.5% / 0.808)',
+  crimsonA11: 'hsl(336 99.8% 40.3% / 0.883)',
+  crimsonA12: 'hsl(340 99.0% 10.0% / 0.950)',
+};
