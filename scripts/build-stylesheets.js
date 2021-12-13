499d4897b3aafd6fdf8ca16770100812015d8d00
Tushar Choudhari <33191895+chtushar@users.noreply.github.com>
2021-12-13 22:56:59 +0530

    :fire: Remove :root selector for the scss builds

--- scripts/build-stylesheets.js
+++ scripts/build-stylesheets.js
@@ -5,13 +5,14 @@ const allColorScales = require("../index");
 const outputDir = require("../tsconfig.json").compilerOptions.outDir;
 
 Object.entries(allColorScales).forEach(([colorScaleName, scale]) => {
-  const selector = /DarkA?$/.test(colorScaleName) ? ".dark-theme" : ":root";
+  const isDarkScale = /DarkA?$/.test(colorScaleName);
 
   // Building css modules
+  const selectorForCSS = isDarkScale ? ".dark-theme" : ":root";
   const scaleAsCssProperties = Object.entries(scale)
     .map(([name, value]) => `  --${name}: ${value};`)
     .join("\n");
-  const scaleAsCssFile = `${selector} {\n${scaleAsCssProperties}\n}`;
+  const scaleAsCssFile = `${selectorForCSS} {\n${scaleAsCssProperties}\n}`;
   fs.writeFileSync(
     path.join(outputDir, colorScaleName + ".css"),
     scaleAsCssFile
@@ -21,7 +22,9 @@ Object.entries(allColorScales).forEach(([colorScaleName, scale]) => {
   const scaleAsScssProperties = Object.entries(scale)
     .map(([name, value]) => `  $${name}: ${value};`)
     .join("\n");
-  const scaleAsScssFile = `${selector} {\n${scaleAsScssProperties}\n}`;
+  const scaleAsScssFile = isDarkScale
+    ? `.dark-theme {\n${scaleAsScssProperties}\n}`
+    : scaleAsScssProperties;
   fs.writeFileSync(
     path.join(outputDir, colorScaleName + ".scss"),
     scaleAsScssFile
