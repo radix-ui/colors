0a29e96544d7714f820a7960f7598273ee512fbb
Anurag <kr.anurag24@gmail.com>
2022-09-07 20:27:05 +0530

    chore: remote comments

--- scripts/gen-colors-object.js
+++ scripts/gen-colors-object.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 
-// generate an array of all file names in a directory
 const getFiles = (dir, files_) => {
   files_ = files_ || [];
   const files = fs.readdirSync(dir);
@@ -16,25 +15,19 @@ const getFiles = (dir, files_) => {
 };
 
 const arr = getFiles("../src/dark");
-// console.log(arr);
 
 let newSmth = [];
 let filesArr = [];
 arr.map((file) => {
   const name = file.split("/").pop().split(".")[0] + "Dark";
-  // filesArr.push(name);
 
   let smth = file.split("/").pop().split(".")[0];
-  // find last letter of the file name
   let lastLetter = smth[smth.length - 1];
 
   if (lastLetter === "A") {
-    // map through all letters of smth
     smth.split("").map((letter, index) => {
-      // if letter is "A" then replace it with "Dark"
       if (letter === "A") {
         smth = smth.replace(letter, "Dark");
-        // console.log(smth);
         newSmth.push(smth + "A");
       }
     });
