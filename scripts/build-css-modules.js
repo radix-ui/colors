878dd0a118db8151b854979a2a7001ee6493bcec
Vlad Moroz <hi@vladmoroz.com>
2023-09-08 12:02:57 +0200

    Build CSS with P3, use current vals

--- scripts/build-css-modules.js
+++ scripts/build-css-modules.js
@@ -4,25 +4,62 @@ const allColorScales = require("../index");
 
 const outputDir = require("../tsconfig.json").compilerOptions.outDir;
 
-Object.entries(allColorScales).forEach(([colorScaleName, scale]) => {
-  const selector = /blackA|whiteA/.test(colorScaleName)
-    ? ":root"
-    : /DarkA?$/.test(colorScaleName)
-    ? ".dark, .dark-theme"
-    : ":root, .light, .light-theme";
+const supportsP3AtRule = "@supports (color: color(display-p3 1 1 1))";
+const matchesP3MediaRule = "@media (color-gamut: p3)";
 
-  const scaleAsCssProperties = Object.entries(scale)
-    .map(([name, value]) => [toCssCasing(name), value])
-    .map(([name, value]) => `  --${name}: ${value};`)
-    .join("\n");
+Object.keys(allColorScales)
+  .filter((key) => !key.includes("P3"))
+  .forEach((key) => {
+    let selector = ":root, .light, .light-theme";
 
-  const scaleAsCssFile = `${selector} {\n${scaleAsCssProperties}\n}`;
+    if (key.includes("Dark")) {
+      selector = ".dark, .dark-theme";
+    }
 
-  fs.writeFileSync(
-    path.join(outputDir, toFileName(colorScaleName) + ".css"),
-    scaleAsCssFile
-  );
-});
+    const isNeutral = key === "blackA" || key === "whiteA";
+
+    if (isNeutral) {
+      selector = ":root";
+    }
+
+    const srgbValues = Object.entries(allColorScales).find(
+      ([name]) => name === key
+    )[1];
+
+    const srgbCssProperties = Object.entries(srgbValues)
+      .map(([name, value]) => [toCssCasing(name), value])
+      .map(([name, value]) => `  --${name}: ${value};`)
+      .join("\n");
+
+    const srgbCssRule = `${selector} {\n${srgbCssProperties}\n}`;
+
+    if (isNeutral) {
+      fs.writeFileSync(
+        path.join(outputDir, toFileName(key) + ".css"),
+        srgbCssRule
+      );
+
+      return;
+    }
+
+    const p3Values = Object.entries(allColorScales).find(
+      ([name]) => name === key + "P3" || name === key.replace(/.$/, "P3A")
+    )[1];
+
+    const p3CssProperties = Object.entries(p3Values)
+      .map(([name, value]) => [toCssCasing(name), value])
+      .map(([name, value]) => `      --${name}: ${value};`)
+      .join("\n");
+
+    let p3CssRule = `    ${selector} {\n${p3CssProperties}\n    }`;
+    p3CssRule = `  ${matchesP3MediaRule} {\n${p3CssRule}\n  }`;
+    p3CssRule = `${supportsP3AtRule} {\n${p3CssRule}\n}`;
+
+    fs.writeFileSync(
+      path.join(outputDir, toFileName(key) + ".css"),
+      `${srgbCssRule}\n\n${p3CssRule}`
+    );
+  });
 
 function toCssCasing(str) {
   return str
